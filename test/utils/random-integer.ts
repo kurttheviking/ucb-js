7dbd78a2cda99f99e5dd9cc8e13db16392187637
Robert Plummer <robertleeplummerjr@gmail.com>
2022-12-08 15:21:55 -0500

    fix: Rebase

--- test/utils/random-integer.ts
+++
@@ -1,7 +0,0 @@
-import { Random, MersenneTwister19937 } from "random-js";
-
-const random = new Random(MersenneTwister19937.autoSeed());
-
-export const randomInteger = (min: number, max: number): number => {
-  return random.integer(min, max);
-}
--- test/utils/repeat-function.ts
+++
@@ -1,8 +0,0 @@
-export const repeatFunction = (x: number) => {
-  return (f: () => void) => {
-    if (x > 0) {
-      f();
-      repeatFunction(x - 1)(f);
-    }
-  };
-};
